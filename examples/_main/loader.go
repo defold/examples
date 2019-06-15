7b2e0543500b267bfe7bc075d905324b65be0f73
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-06-15 23:37:59 +0200

    Make sure to reset projection when unloading example

--- examples/_main/loader.go
+++ examples/_main/loader.go
@@ -514,3 +514,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "render/camera"
+  type: "collectionproxy"
+  data: "collection: \"/examples/render/camera/camera.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
