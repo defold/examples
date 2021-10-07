e0c47b4caf10498865f9bdba8a3e85d90c5c53af
Paweł Jarosz <pawel.jarosz21@gmail.com>
2021-10-07 15:30:30 +0200

    Added cursor animation example

--- examples/_main/loader.go
+++ examples/_main/loader.go
@@ -766,3 +766,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sprite/cursor"
+  type: "collectionproxy"
+  data: "collection: \"/examples/sprite/cursor/cursor.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
