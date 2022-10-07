7ae99c0e559b25cb0588185297230d66e5becbd1
Pawel Jarosz <pawel.jarosz21@gmail.com>
2022-10-07 15:31:43 +0200

    Added trigger timer example to loader.

--- examples/_main/loader.go
+++ examples/_main/loader.go
@@ -946,3 +946,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "timer/trigger_timer"
+  type: "collectionproxy"
+  data: "collection: \"/examples/timer/trigger_timer/trigger_timer.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
